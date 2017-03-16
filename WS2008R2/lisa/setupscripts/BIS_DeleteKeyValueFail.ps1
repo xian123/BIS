ce581f04c59ef1f109addc0932336fcf993226b9
Hongxiong Xian (Wicresoft) <v-hoxian@microsoft.com>
2017-03-16 16:55:09 +0800

    Update the test case of DeleteNonExistentKvpDataOnGuest

--- WS2008R2/lisa/setupscripts/BIS_DeleteKeyValueFail.ps1
+++ WS2008R2/lisa/setupscripts/BIS_DeleteKeyValueFail.ps1
@@ -164,19 +164,63 @@ while($job.jobstate -lt 7) {
 	$job.get()
 } 
 
-if ($job.ErrorCode -eq 0)
-{
-    "Error: The job should not succeed while deleting a non-existent key value pair."
-    return $False;
-}
+Write-Output $job.ErrorCode
+Write-Output $job.Status
 
-if ($job.ErrorCode -eq 32773)
-{  
-    "Info: Key does not exist."
-    return $True
-}
-else
+"Info : Detecting Host version of Windows Server"
+$osInfo = GWMI Win32_OperatingSystem -ComputerName $hvServer
+if (-not $osInfo)
 {
-    "Error: Unknown error code."
+    "Error: Unable to collect Operating System informatioin"
     return $False
 }
+
+#
+# Due to a change in behavior between Server 2012 and 2012 R2, we need to modify
+# acceptance criteria based on the version of the HyperVisor.
+#
+switch ($osInfo.BuildNumber)
+{
+    "7601" # Server 2008 R2
+    {
+        if ($job.ErrorCode -eq 32773)
+        {
+            "Info : RemoveKvpItems() correctly returned 32773"
+            return $True
+        }
+        "Error: RemoveKVPItems() returned error code $($job.ErrorCode) rather than 32773"
+        return $False
+    }
+
+	"9200" # Server 2012
+	{
+		if ($job.ErrorCode -eq 32773)
+		{
+			"Info : RemoveKvpItems() correctly returned 32773"
+			return $True
+		}
+		"Error: RemoveKVPItems() returned error code $($job.ErrorCode) rather than 32773"
+		return $False
+	}
+
+	"9600" # Server 2012 R2
+	{
+		if ($job.ErrorCode -eq 0)
+		{
+			"Info : Server 2012 R2 returns success even when the KVP item does not exist"
+			return $True
+		}
+		"Error: RemoveKVPItems() returned error code $($job.ErrorCode)"
+		return $False
+	}
+	
+    Default # An unsupported version of Windows Server
+	{
+		#
+		# We should only hit this case when testing on Windows.Next
+		#
+		"Error: Unsupported build of Windows Server"
+		return $False
+	}
+}
+
